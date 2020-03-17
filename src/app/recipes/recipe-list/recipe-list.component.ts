19cc08e50dae9acebc1ee9cc0287f5f9211fdc12
Greg <gknudsen@duqlight.com>
2020-03-17 16:51:40 -0400

    add recipe model/list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from './../recipe.model';
+
 @Component({
   selector: 'recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test recipe', 'a test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg')
+  ];
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    console.log(this.recipes);
   }
 
 }
